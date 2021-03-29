a3e336bed3e863424dc2f39d973a15fcf180df32
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-03-30 00:38:00 +0300

    fix shellcheck warning

--- ci-scripts/tag.sh
+++ ci-scripts/tag.sh
@@ -2,9 +2,9 @@
 
 function increment_minor_version() {
     local version_major, version_minor, version_patch
-    version_major=$(echo $1 | cut -d "." -f 1)
-    version_minor=$(echo $1 | cut -d "." -f 2)
-    version_patch=$(echo $1 | cut -d "." -f 3)
+    version_major=$(echo "$1" | cut -d "." -f 1)
+    version_minor=$(echo "$1" | cut -d "." -f 2)
+    version_patch=$(echo "$1" | cut -d "." -f 3)
     # shellcheck disable=SC2003
     version_patch=$(expr "${version_patch}" + 1)
     echo "${version_major}.${version_minor}.${version_patch}"
@@ -13,7 +13,7 @@ function increment_minor_version() {
 function set_version() {
     sed -i "s/${1}/${2}/g" version.properties
 }
-
+ö
 function set_chart_version() {
     sed -i "s/${1}/${2}/g" charts/"${CHART_NAME}"/Chart.yaml
     sed -i "s/${1}/${2}/g" charts/"${CHART_NAME}"/values.yaml
