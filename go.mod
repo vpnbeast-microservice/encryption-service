1e5363d8bc69b81eac50e946870170083db4178b
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-08-04 01:19:39 +0300

    move initOptions to commons

--- go.mod
+++ go.mod
@@ -6,11 +6,6 @@ require (
 	github.com/golang/gddo v0.0.0-20210115222349-20d68f94ee1f
 	github.com/gorilla/mux v1.8.0
 	github.com/prometheus/client_golang v1.11.0
-	github.com/spf13/viper v1.8.1
-	github.com/vpnbeast/golang-commons v0.0.4
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.7.0 // indirect
+	github.com/vpnbeast/golang-commons v0.0.6
 	go.uber.org/zap v1.18.1
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
-	golang.org/x/text v0.3.6 // indirect
 )
--- pkg/options/options.go
+++ pkg/options/options.go
@@ -1,11 +1,8 @@
 package options
 
 import (
-	"fmt"
-	"github.com/spf13/viper"
 	commons "github.com/vpnbeast/golang-commons"
 	"go.uber.org/zap"
-	"net/http"
 )
 
 var (
@@ -16,7 +13,7 @@ var (
 func init() {
 	logger = commons.GetLogger()
 	opts = newEncryptionServiceOptions()
-	err := opts.initOptions()
+	err := commons.InitOptions(opts, "encryption-service")
 	if err != nil {
 		logger.Fatal("fatal error occured while initializing options", zap.Error(err))
 	}
@@ -41,51 +38,4 @@ type EncryptionServiceOptions struct {
 	ReadTimeoutSeconds  int    `env:"READ_TIMEOUT_SECONDS"`
 	// encryption related config
 	Secret string `env:"SECRET"`
-}
-
-// initOptions initializes EncryptionServiceOptions while reading environment values, sets default values if not specified
-func (eso *EncryptionServiceOptions) initOptions() error {
-	activeProfile := commons.GetStringEnv("ACTIVE_PROFILE", "local")
-	appName := commons.GetStringEnv("APP_NAME", "encryption-service")
-	// TODO: below if/else logic can be implemented using library to decrease duplicate code across other projects?
-	if activeProfile == "unit-test" {
-		logger.Info("active profile is unit-test, reading configuration from static file")
-		// TODO: better approach for that?
-		viper.AddConfigPath("./../../config")
-		viper.SetConfigName("unit_test")
-		viper.SetConfigType("yaml")
-		if err := viper.ReadInConfig(); err != nil {
-			return err
-		}
-	} else {
-		configHost := commons.GetStringEnv("CONFIG_SERVER_HOST", "localhost")
-		configPort := commons.GetIntEnv("CONFIG_SERVER_PORT", 8888)
-		logger.Info("loading configuration from remote server", zap.String("host", configHost),
-			zap.Int("port", configPort), zap.String("appName", appName),
-			zap.String("activeProfile", activeProfile))
-		confAddr := fmt.Sprintf("http://%s:%d/%s-%s.yaml", configHost, configPort, appName, activeProfile)
-		resp, err := http.Get(confAddr)
-		if err != nil {
-			return err
-		}
-
-		defer func() {
-			err := resp.Body.Close()
-			if err != nil {
-				panic(err)
-			}
-		}()
-
-		viper.SetConfigName("application")
-		viper.SetConfigType("yaml")
-		if err = viper.ReadConfig(resp.Body); err != nil {
-			return err
-		}
-	}
-
-	if err := commons.UnmarshalConfig(appName, eso); err != nil {
-		return err
-	}
-
-	return nil
 }
